5cdc9791aa655db222fc304aa8c7d8c92ea11aef
thedoubleace <32985361+thedoubleace@users.noreply.github.com>
2022-08-19 21:17:59 -0300

    feat: geração de cogumelos, colisão com cogumelos

--- main.c
+++ main.c
@@ -5,10 +5,10 @@
 #include <string.h>
 
 #define ENTER 10
-#define LARGURA_TELA 800
+#define LARGURA_TELA 810
 #define ALTURA_TELA 640
 
-#define MOVIMENTO 10.0
+#define MOVIMENTO 4
 #define ESC 27
 
 #define DIMENSAO_RETANGULO_BORDA 5
@@ -18,8 +18,12 @@
 #define MARGEM_JOGO_X 10
 
 #define TAMANHO_FONTE 30
-#define TAMANHO_JOGADOR 15
 #define TAMANHO_STR 20
+
+#define TAMANHO_JOGADOR 15
+#define TAMANHO_COGUMELO 15
+
+#define MARGEM_COGUMELO 5
 
 #define NUM_ITEMS_MENU 4
 
@@ -28,7 +32,6 @@
 #define NUM_TIROS 200
 
 void desenha_moldura() {
-
     int i;
 
     for(i = MARGEM_JOGO_X; i < LARGURA_TELA - MARGEM_JOGO_X; i++) {
@@ -61,7 +64,6 @@ void desenha_menu_superior(char itens_menu[][TAMANHO_STR]){
 }
 
 void desenha_menu_inferior(char itens_menu[][TAMANHO_STR], int pontos, int cogumelos_restantes, int vidas, int tiros){
-
     int i;
     float pos = 0;
 
@@ -78,40 +80,44 @@ void desenha_menu_inferior(char itens_menu[][TAMANHO_STR], int pontos, int cogum
         } else {
             DrawText(itens_menu[i], MARGEM_JOGO_X + (LARGURA_TELA) * pos, ALTURA_TELA - MARGEM_JOGO_Y + 10, TAMANHO_FONTE, WHITE);
         }
-
         pos = pos + 0.125;
     }
 }
 
-void movimenta_jogador(int *posicao_x, int *posicao_y) {
+int verifica_colisao_cogumelos(int posicao_x, int posicao_y, int tamanho_objeto, int pos_cogumelos[][2]){
+    int i = 0, flag = 0;
+
+    for(i = 0; i < NUM_COGUMELOS; i++){
+        if((posicao_x < pos_cogumelos[i][0] + TAMANHO_COGUMELO) && (posicao_x + TAMANHO_JOGADOR > pos_cogumelos[i][0]) && posicao_y < pos_cogumelos[i][1] + TAMANHO_COGUMELO && posicao_y + TAMANHO_JOGADOR > pos_cogumelos[i][1]){
+            flag = 1;
+        }
+    }
+    return flag;
+}
+
+int verifica_movimento(int posicao_x, int posicao_y, int pos_cogumelos[][2]){
+    // Verifica se é possível o jogador fazer algum movimento. !!!Recebe a nova posicao, nao a atual!!!
+    // Retorna 1 se o movimento é possível, 0 se não
+    int flag = 1;
+
+    if(posicao_x > LARGURA_TELA - MARGEM_JOGO_X - TAMANHO_JOGADOR){
+        flag = 0; // Verifica se o jogador ultrapassa a parede da direita
+    } else if(posicao_x < MARGEM_JOGO_X + DIMENSAO_RETANGULO_BORDA) {
+        flag = 0; // Verifica se o jogador ultrapassa a parede da esquerda
+    } else if(posicao_y < ((ALTURA_TELA - MARGEM_JOGO_Y) * 0.75)){
+        flag = 0; // Verifica se o jogador vai acima do 1/4 inferior da tela
+    } else if(posicao_y > (ALTURA_TELA - MARGEM_JOGO_Y - TAMANHO_JOGADOR)){
+        flag = 0; // Verifica se o jogador ultrapassa a parede de baixo
+    } else if(verifica_colisao_cogumelos(posicao_x, posicao_y, TAMANHO_JOGADOR, pos_cogumelos)){
+        flag = 0; // Verifica se o jogador colide com algum cogumelo
+    }
+
+    return flag;
+}
+
+void movimenta_jogador(int *posicao_x, int *posicao_y, int pos_cogumelos[][2]) {
 
     switch(GetKeyPressed()) {
-        case KEY_RIGHT:
-            if (*posicao_x < LARGURA_TELA - MARGEM_JOGO_X - TAMANHO_JOGADOR - DIMENSAO_RETANGULO_BORDA) {
-                *posicao_x += MOVIMENTO;
-            }
-
-            break;
-        case KEY_LEFT:
-            if (*posicao_x > MARGEM_JOGO_X + DIMENSAO_RETANGULO_BORDA) {
-                *posicao_x -= MOVIMENTO;
-            }
-
-            break;
-        case KEY_UP:
-            // Regra para o jogador se mover somente no quarto inferior da tela
-            if (*posicao_y > ((ALTURA_TELA - MARGEM_JOGO_Y) * 0.75)) {
-                *posicao_y -= MOVIMENTO;
-            }
-
-            break;
-        case KEY_DOWN:
-            if (*posicao_y > 0 && *posicao_y < ALTURA_TELA - MARGEM_JOGO_Y - TAMANHO_JOGADOR - DIMENSAO_RETANGULO_BORDA) {
-                *posicao_y += MOVIMENTO;
-            }
-
-            break;
-
         case ESC:
             CloseWindow();
             break;
@@ -119,17 +125,66 @@ void movimenta_jogador(int *posicao_x, int *posicao_y) {
         default:
             break;
     }
+
+    if(IsKeyDown(KEY_RIGHT)){
+        if (verifica_movimento(*posicao_x + MOVIMENTO, *posicao_y, pos_cogumelos)) {
+            *posicao_x += MOVIMENTO;
+        }
+    }
+    if (IsKeyDown(KEY_LEFT)){
+        if (verifica_movimento(*posicao_x - MOVIMENTO, *posicao_y, pos_cogumelos)) {
+            *posicao_x -= MOVIMENTO;
+        }
+    }
+    if (IsKeyDown(KEY_UP)){
+        if (verifica_movimento(*posicao_x, *posicao_y - MOVIMENTO, pos_cogumelos)) {
+            *posicao_y -= MOVIMENTO;
+        }
+    }
+    if (IsKeyDown(KEY_DOWN)){
+        if (verifica_movimento(*posicao_x, *posicao_y + MOVIMENTO, pos_cogumelos)) {
+            *posicao_y += MOVIMENTO;
+        }
+    }
 }
 
+int verifica_posicao_cogumelos(int pos_cogumelos[][2], int tamanho_array, int x, int y){
+    int i = 0, flag = 0;
 
-int gera_posicao_random(int valor_maximo) {
-    return (int) (rand() / (double) RAND_MAX * (valor_maximo + 1));
+    // Se as coordenadas recebidas já estão ocupadas por um cogumelo, retorna 1, senão 0. Para de percorrer o array quando chega ao final, ou encontra o valor 0.
+    while(i < tamanho_array && pos_cogumelos[i][0] != 0){
+        if (pos_cogumelos[i][0] == x && pos_cogumelos[i][1] == y){
+            flag = 1;
+        }
+        i++;
+    }
+    return flag;
 }
 
+int gera_posicao_random(int valor_minimo, int valor_maximo) {
+    // Gera valores aleatórios entre o valor minimo e maximo
+    return (int) (rand() % (valor_maximo - valor_minimo + 1) + valor_minimo);
+}
 
-void gera_cogumelos() {
-    // gerar N cogumelos chamando gera_posicao_random para X e Y
-    // gera_posicao_random receberia largura e (altura * 0.75) da tela como maximos para X e Y
+int gera_posicao_cogumelos(int valor_minimo, int valor_maximo) {
+    // Gera valores aleatórios, divisiveis pelo tamanho dos cogumelos + sua margem (para eles ficarem em seus próprios tiles)
+    return (int) (rand() % (valor_maximo - valor_minimo + 1) + valor_minimo) / (TAMANHO_COGUMELO + MARGEM_COGUMELO) * (TAMANHO_COGUMELO + MARGEM_COGUMELO);
+}
+
+void gera_cogumelos(int pos_cogumelos[][2]) {
+    int i = 0, x, y;
+    srand(time(0));
+
+    while (i < NUM_COGUMELOS){
+        x = gera_posicao_cogumelos(MARGEM_JOGO_X + DIMENSAO_RETANGULO_BORDA + TAMANHO_COGUMELO, LARGURA_TELA - MARGEM_JOGO_X - DIMENSAO_RETANGULO_BORDA);
+        y = gera_posicao_cogumelos(MARGEM_JOGO_Y + DIMENSAO_RETANGULO_BORDA + TAMANHO_COGUMELO * 2, ALTURA_TELA - MARGEM_JOGO_Y - DIMENSAO_RETANGULO_BORDA - TAMANHO_JOGADOR - TAMANHO_COGUMELO);
+        // Verifica se a posição já está ocupada. Se não, gera uma nova posição para o cogumelo.
+        if (!verifica_posicao_cogumelos(pos_cogumelos, NUM_COGUMELOS, x, y)){
+            pos_cogumelos[i][0] = x;
+            pos_cogumelos[i][1] = y;
+            i++;
+        }
+    }
 }
 
 void desenha_jogador(int *posicao_x, int *posicao_y) {
@@ -138,19 +193,30 @@ void desenha_jogador(int *posicao_x, int *posicao_y) {
     DrawRectangle(*posicao_x, *posicao_y, TAMANHO_JOGADOR, TAMANHO_JOGADOR, RED);
 }
 
+void desenha_cogumelos(int pos_cogumelos[][2]){
+    int i;
+
+    for (i = 0; i < NUM_COGUMELOS; i++){
+        DrawRectangle(pos_cogumelos[i][0], pos_cogumelos[i][1], TAMANHO_COGUMELO, TAMANHO_COGUMELO, GREEN);
+    }
+}
+
 int main() {
 
-    int posicao_x = LARGURA_TELA / 2, posicao_y = ALTURA_TELA * 0.80;
+    int posicao_x = LARGURA_TELA / 2, posicao_y = ALTURA_TELA - MARGEM_JOGO_Y - DIMENSAO_RETANGULO_BORDA - TAMANHO_JOGADOR;
     int pontos = 0, cogumelos_restantes = NUM_COGUMELOS, vidas = NUM_VIDAS, tiros = NUM_TIROS;
 
     char itens_menu_superior[NUM_ITEMS_MENU][TAMANHO_STR] = {"ESC-Sair", "C-Carregar", "P-Pausar", "R-Ranking"};
     char itens_menu_inferior[NUM_ITEMS_MENU * 2][TAMANHO_STR] = {"PTS", "", "COG", "", "VDS", "", "TRS", ""};
 
+    int pos_cogumelos[NUM_COGUMELOS][2] = {};
+    gera_cogumelos(pos_cogumelos);
+
     InitWindow(LARGURA_TELA, ALTURA_TELA, "millipede");
-    SetTargetFPS(15);
+    SetTargetFPS(60);
 
     while (!WindowShouldClose()) {
-        movimenta_jogador(&posicao_x, &posicao_y);
+        movimenta_jogador(&posicao_x, &posicao_y, pos_cogumelos);
 
         BeginDrawing();
         ClearBackground(BLACK);
@@ -159,9 +225,8 @@ int main() {
         desenha_menu_superior(itens_menu_superior);
         desenha_menu_inferior(itens_menu_inferior, pontos, cogumelos_restantes, vidas, tiros);
 
-        gera_cogumelos();
-
         desenha_jogador(&posicao_x, &posicao_y);
+        desenha_cogumelos(pos_cogumelos);
 
         EndDrawing();
     }
