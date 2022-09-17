87e80abe12585797446148d074a6350c6c03b5d2
Lucas Rohr Carreno <lucasrc17@live.com>
2022-09-17 13:14:39 -0300

    chore: :bug: fixing param

--- main.c
+++ main.c
@@ -14,55 +14,6 @@
 #include "aranhas.h"
 #include "saves.h"
 
-void game_loop(FAZENDEIRO *fazendeiro, COGUMELO cogumelos[], ARANHA aranhas[], JOGADOR jogadores[], MILIPEDE *milipede){
-
-    STATUS_JOGO status_jogo = normal;
-    int sair = 0;
-
-    char input[TAMANHO_NOME + 1] = "\0";
-    int num_letras = 0;
-    char itens_menu_superior[NUM_ITEMS_MENU][TAMANHO_STR] = {"ESC-Sair", "C-Carregar", "P-Pausar", "R-Ranking"};
-    char itens_menu_inferior[NUM_ITEMS_MENU * 2][TAMANHO_STR] = {"PTS", "", "COG", "", "VDS", "", "TRS", ""};
-
-    while (!WindowShouldClose() && !sair) {
-        BeginDrawing();
-        ClearBackground(BLACK);
-
-        desenha_moldura();
-        desenha_menu_superior(itens_menu_superior);
-        desenha_menu_inferior(itens_menu_inferior, *fazendeiro, NUM_COGUMELOS);
-
-        desenha_tiros(fazendeiro->tiros, NUM_TIROS);
-        desenha_jogador(*fazendeiro);
-        desenha_cogumelos(cogumelos, NUM_COGUMELOS);
-        desenha_aranhas(aranhas, NUM_ARANHAS);
-        desenha_milipede(*milipede);
-
-        comandos_jogador(fazendeiro, aranhas, cogumelos, NUM_COGUMELOS, jogadores, &status_jogo);
-
-        if (status_jogo == pausado){ // Comandos se o jogo esta pausado
-            menu_pausa(fazendeiro, aranhas, cogumelos, input, &num_letras, &status_jogo);
-        } else if(status_jogo == carregando){ // Comandos se o jogo esta carregando
-            menu_carregar(fazendeiro, aranhas, cogumelos, input, &num_letras, &status_jogo);
-        } else if(status_jogo == mostrando_ranking){ // Comandos se o ranking está sendo mostrado
-            desenha_ranking(jogadores);
-        } else if(status_jogo == saindo){ // Comandos se o jogador quer sair
-            menu_sair(fazendeiro, jogadores, input, &num_letras, &status_jogo, &sair);
-        } else{
-            // Comandos se nao
-            movimenta_jogador(fazendeiro, cogumelos);
-            move_aranhas(fazendeiro, aranhas, cogumelos, NUM_ARANHAS);
-            movimenta_tiros(fazendeiro->tiros);
-            verifica_tiros(fazendeiro, cogumelos);
-            verifica_tiros_milipede(fazendeiro, milipede);
-            movimenta_milipede(milipede, cogumelos, NUM_COGUMELOS, fazendeiro);
-        }
-
-        EndDrawing();
-    }
-
-}
-
 // Funcoes para Milipede
 
 void gera_milipede(MILIPEDE *milipede) {
@@ -237,6 +188,55 @@ void verifica_tiros_milipede(FAZENDEIRO *fazendeiro, MILIPEDE *milipede){
 
 /// Fim das Funcoes para Milipede
 
+void game_loop(FAZENDEIRO *fazendeiro, COGUMELO cogumelos[], ARANHA aranhas[], JOGADOR jogadores[], MILIPEDE *milipede){
+
+    STATUS_JOGO status_jogo = normal;
+    int sair = 0;
+
+    char input[TAMANHO_NOME + 1] = "\0";
+    int num_letras = 0;
+    char itens_menu_superior[NUM_ITEMS_MENU][TAMANHO_STR] = {"ESC-Sair", "C-Carregar", "P-Pausar", "R-Ranking"};
+    char itens_menu_inferior[NUM_ITEMS_MENU * 2][TAMANHO_STR] = {"PTS", "", "COG", "", "VDS", "", "TRS", ""};
+
+    while (!WindowShouldClose() && !sair) {
+        BeginDrawing();
+        ClearBackground(BLACK);
+
+        desenha_moldura();
+        desenha_menu_superior(itens_menu_superior);
+        desenha_menu_inferior(itens_menu_inferior, *fazendeiro, NUM_COGUMELOS);
+
+        desenha_tiros(fazendeiro->tiros, NUM_TIROS);
+        desenha_jogador(*fazendeiro);
+        desenha_cogumelos(cogumelos, NUM_COGUMELOS);
+        desenha_aranhas(aranhas, NUM_ARANHAS);
+        desenha_milipede(*milipede);
+
+        comandos_jogador(fazendeiro, aranhas, cogumelos, NUM_COGUMELOS, jogadores, &status_jogo);
+
+        if (status_jogo == pausado){ // Comandos se o jogo esta pausado
+            menu_pausa(fazendeiro, aranhas, cogumelos, input, &num_letras, &status_jogo);
+        } else if(status_jogo == carregando){ // Comandos se o jogo esta carregando
+            menu_carregar(fazendeiro, aranhas, cogumelos, input, &num_letras, &status_jogo);
+        } else if(status_jogo == mostrando_ranking){ // Comandos se o ranking está sendo mostrado
+            desenha_ranking(jogadores);
+        } else if(status_jogo == saindo){ // Comandos se o jogador quer sair
+            menu_sair(fazendeiro, jogadores, input, &num_letras, &status_jogo, &sair);
+        } else{
+            // Comandos se nao
+            movimenta_jogador(fazendeiro, cogumelos);
+            move_aranhas(fazendeiro, aranhas, cogumelos, NUM_ARANHAS);
+            movimenta_tiros(fazendeiro->tiros);
+            verifica_tiros(fazendeiro, cogumelos);
+            verifica_tiros_milipede(fazendeiro, milipede);
+            movimenta_milipede(milipede, cogumelos, NUM_COGUMELOS, fazendeiro);
+        }
+
+        EndDrawing();
+    }
+
+}
+
 int main() {
     FAZENDEIRO fazendeiro = {};
     COGUMELO cogumelos[NUM_COGUMELOS] = {};
@@ -256,7 +256,7 @@ int main() {
     SetExitKey(KEY_F1); // Para liberar o ESC, default da raylib
     // EnableEventWaiting();
 
-    game_loop(&fazendeiro, cogumelos, aranhas, jogadores);
+    game_loop(&fazendeiro, cogumelos, aranhas, jogadores, &milipede);
 
     CloseWindow();
 
